532a28d74a9f621035ec9ae031c87f6b0f9210f2
Thalles José <thallesjosesv@gmail.com>
2024-07-24 21:36:28 -0300

    reponsivite

--- assets/script/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-    var loader = document.getElementById("loader");
-    var content = document.getElementById("content");
-    
-    loader.style.display = "none";
-    content.style.display = "block";
-  });
